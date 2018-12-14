8e21ccb276f0db4fb5910dba652173ec637b1c1f
Lin Jngxian <14100634@qq.com>
2018-12-14 19:54:40 +0800

    refactor

--- controllers/wxWeb/api/exchangeShopHandler.js
+++ controllers/wxWeb/api/exchangeShopHandler.js
@@ -40,35 +40,5 @@ pro.exchangeItem = async function(ctx, next){
     let ret = await app.userCenter.exchangeItem(openId,itemId,amount);
     ctx.body = ret;
 }
-/**
- * 发送消息给用户
- */
-pro.sendMessage = async function(ctx, next){
-    let app = ctx.app;
-    let receiver = ctx.session.uid;
-    let title = ctx.request.body.title || '';
-    let content = ctx.request.body.content || '';
-    let ret = await app.userCenter.sendMessage(receiver,title,content);
-    ctx.body = ret;    
-};
-/**
- * 标注用户已经完成阅读
- */
-pro.markMessageRead = async function(ctx,next){
-    let app = ctx.app;
-    let receiver = ctx.session.uid;
-    let msgId = ctx.request.body.msgId;
-    let ret = await app.userCenter.markMessageRead(receiver,msgId);
-    ctx.body = ret;
-};
-/**
- * 删除指定消息
- */
-pro.deleteMessage = async function(ctx,next){
-    let app = ctx.app;
-    let receiver = ctx.session.uid;
-    let msgIds = ctx.request.body.msgIds.split(';');  // 使用分号分隔
-    let ret = await app.userCenter.deleteMessage(receiver,msgIds);
-    ctx.body = ret;
-}
+
 module.exports = ExchangeShopHandler;
